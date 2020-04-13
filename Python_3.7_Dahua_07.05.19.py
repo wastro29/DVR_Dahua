4fd8256cab81e634ad7d22c54ef0a4825dbe6473
wastro <10563581+wastro29@users.noreply.github.com>
2020-04-13 15:33:07 +0700

    Update Python_3.7_Dahua_07.05.19.py

--- Python_3.7_Dahua_07.05.19.py
+++ Python_3.7_Dahua_07.05.19.py
@@ -57,23 +57,30 @@ def Time_conv(Time):
     return data
 
 def carve_file(f, blocksize, quality, Spath):
-    SignStr = b'\x44\x48\x41\x56\xFD'
+    SignStr = b'44484156FD'
 #    SignStr = b'\x44\x48\x41\x56\xFC'
-    SignEnd = b'\x64\x68\x61\x76'
-    regexStr = re.compile(SignStr)
+#    SignEnd = b'\x64\x68\x61\x76'
+    bin_str = bs.unhexlify(sign_str)
+    regexStr = re.compile(bin_str)
     i = 0
     k = 0
     l = -1
     c = 0
     StartOffset = 0
     EndOffset = 0
-    FirstCam = b'\x00'
+    FirstCam = bytes(1)
     m = hashlib.md5()
     SOF_List = ['start']
     EOF_List = ['start-21']
     Time_List = ['Time']
     Cam_List = ['Cam']
     Qual_List = ['Quality']
+    if os.name == 'nt':
+        Cam_n = '\Cam_'
+    else:
+        Cam_n = '/Cam_'
+    fl = '\log.txt'
+    t = f'{Spath}{fl}'
     while True:
         buf = f.read(blocksize)
         l = l + 1
@@ -83,7 +90,7 @@ def carve_file(f, blocksize, quality, Spath):
 
         for match_obj in regexStr.finditer(buf):
             offsetSt = match_obj.start()
-            tmp = str(int(l * blocksize + offsetSt))
+            tmp = str(int(l * blocksize + offsetSt + jump))
 #            offsetEd = match_obj.end()
 #            print "hexSt: " + hex(offsetSt)
 #            print "hexEd: " + hex(offsetEd)
@@ -116,12 +123,11 @@ def carve_file(f, blocksize, quality, Spath):
                     EndOffset = offsetSt - 1
                     subdata = buf[StartOffset:EndOffset]
                     if (FirstQual == quality) or (quality == all):
-                        time_s = Time_conv(FirstDate)
-#                        time_e = Time_conv(FirstDate)
+                        time_s = Time_conv(FirstDate_)
+                        time_e = Time_conv(FirstDate)
 
 #                        filename = "N:\start_"+ time_s + '_' + str(int(l * blocksize + StartOffset)) + "_" + str(int(l * blocksize + EndOffset)) + "_" + "Cam_" + str(FirstCam) + '.dav'
-                        filename_ = Spath + "\Cam_" + str(FirstCam) + "_" + Time_conv(FirstDate_) + "-" + Time_conv(FirstDate) + "_"
-                        filename = f'{filename_}' + str(int(l * blocksize + StartOffset+jump)) + "_" + str(int(l * blocksize + EndOffset+jump)) + ".dav"
+                        filename = f'{Spath}{Cam_n}{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}.dav'
 
                         with open(f'{filename}', 'wb') as files:
                              files.write(subdata)
@@ -130,7 +136,19 @@ def carve_file(f, blocksize, quality, Spath):
 #                       copy_file.write(buf)
 #                       copy_file.close()
                         c = 1
-                        print(filename)
+#                        print(filename)
+                        for log in range(1):
+                            if os.path.exists(t):
+                                log = open(t, 'a')
+                                log.write(f'Cam : {FirstCam}\nStart Date : {time_s} End Date : {time_e}\n')
+                                log.write(f'Data File on {Spath} : Cam_{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}.dav\r\n')
+                            else:
+                                log = open(t, 'w')
+                                log.write("== Log data Backup ==\r\n")
+                                log.write(f'Cam :{FirstCam}\nStart Date : {time_s} End Date : {time_e}\n')
+                                log.write(f'Data File on {Spath} : Cam_{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}.dav\r\n')
+                                
+                        log.close()
                     StartOffset = offsetSt
                     FirstCam = int.from_bytes(
                                               buf[offsetSt + 6:offsetSt + 8], byteorder='little')+1
@@ -147,15 +165,33 @@ def carve_file(f, blocksize, quality, Spath):
         if (c == 0)and (StartOffset != 0) and (EndOffset != 0) :
             if (FirstQual == quality) or (quality == all):
                 subdata = buf[StartOffset:EndOffset]
-                filename_ = Spath + "\Cam_" + str(FirstCam) + "_" + Time_conv(FirstDate_) + "-" + Time_conv(FirstDate)+ "_"
-                filename = f'{filename_}' + str(int(l * blocksize + StartOffset+jump)) + "_" + str(int(l * blocksize + EndOffset+jump)) + ".dav"
+                filename = f'{Spath}{Cam_n}{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}_2.dav'
                 with open(f'{filename}', 'wb') as files:
                      files.write(subdata)
                      files.close()
+                for log in range(1):
+                    if os.path.exists(t):
+                        log = open(t, 'a')
+                        log.write(f'Cam : {FirstCam}\nStart Date : {time_s} End Date : {time_e}\n')
+                        log.write(f'Data File  on {Spath} : Cam_{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}_2.dav\r\n')
+                    else:
+                        log = open(t, 'w')
+                        log.write("== Log data Backup ==\r\n")
+                        log.write(f'Cam :{FirstCam}\nStart Date : {time_s} End Date : {time_e}\n')
+                        log.write(f'Data File  on {Spath} : Cam_{FirstCam}_{time_s}_{time_e}-{tmp}_{tmp}_2.dav\r\n')
+                        
+                log.close()
                 StartOffset = 0
                 EndOffset = 0
                 k = k + 1
         print (SOF_List)
+        if os.path.exists(t):
+            log = open(t, 'r')
+            print(log.read())
+            log.close()
+        else:
+            print(f'Waiting Process {t} is empty')
+            
 #        print (Cam_List)
 #        print (Time_List)
 #        print (Qual_List)
@@ -171,12 +207,19 @@ S = input('Location Disk/RAW file : ')
 print(f'{S}')
 f = open(f'{S}', 'rb')  # Work with connected Drive
 jump = 0  # bytes
-f.seek(jump, 0)#move the file pointer forward by jump bytes from start of the file
+f.seek(jump, 0)  # move the file pointer forward by jump bytes from start of the file
 D = input('Location to Save : ')
 print(f'{D}')
-Spath = f'{D}' # Destination path 
-blocksize = 2**30  # Block size
-quality = 12  # Quality of video frames. For all type of quality use 'all' without quotation marks.
+Spath = f'{D}'  # Destination path
+x = input('Blocksize (512/1024/4096) : ')
+y = 3
+blocksize = pow(int(x), int(y))  # Block size
+qual = input('Quality (1-25 / all) : ')
+if f'{qual}' == 'all':
+    quality = all
+else:
+    quality = int(qual)
+# quality = 12  # Quality of video frames. For all type of quality use 'all' without quotation marks.
 print(carve_file(f, blocksize, quality, Spath))  # f- image for analysis;
 # blocksize-block size for analysis;
 # quality - determine by byte with offset 0x1D in the integer from the beginning of the signature 0x44484156FD;
